ccbdad8c03926010680474cc8b552be4dd75568f
Arvind Singh <91238742+Arvind0-0@users.noreply.github.com>
2022-09-03 18:54:30 +0530

    Time: 42 ms (94.56%), Space: 14.1 MB (73.81%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
@@ -1,13 +1,15 @@
 class Solution:
     def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
-        ans = []
-        d = deque((1, d) for d in range(1, 10))
-        while d:
-            pos, num = d.pop()
-            if pos == n:
-                ans.append(num)
-            else:
-                for j in range(10):
-                    if abs(num % 10 - j) == k:
-                        d.append((pos + 1, num * 10 + j))
-        return ans
+        numset = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        
+        for i in range(n - 1):
+            res = []
+            for num in numset:
+                cur = num % 10
+                
+                if cur + k <= 9: res.append(num * 10 + cur + k)
+                if k != 0 and cur - k >= 0: res.append(num * 10 + cur - k)
+                    
+            numset = res
+        
+        return res
