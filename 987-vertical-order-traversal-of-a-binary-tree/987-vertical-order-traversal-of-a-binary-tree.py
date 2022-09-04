e12549ec0e7de7ff3008b5788bec28211fc5bcf3
Arvind Singh <91238742+Arvind0-0@users.noreply.github.com>
2022-09-04 20:32:22 +0530

    Time: 42 ms (80.02%), Space: 14.2 MB (28.65%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
@@ -1,23 +1,24 @@
+from collections import deque
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        results = defaultdict(list)
-        
-        queue = [ (root, 0, 0) ]
-        
-        while queue:
-            node, pos, depth = queue.pop(0)
-            if not node: continue
-            results[(pos,depth)].append(node.val)
-            results[(pos,depth)].sort()
-            queue.extend( [ (node.left, pos-1, depth+1), (node.right, pos+1, depth+1) ] )
-            
-            
-        res = defaultdict(list)
-        keys = sorted(list(results.keys()), key=lambda x: (x[0], x[1]))
-        
-        
-        for k in keys:
-            pos, depth = k
-            res[pos].extend(results[k])
+        col_rows = defaultdict(list)
 
-        return res.values()
+        queue = deque()
+        queue.append((root, 0, 0))
+        while queue:
+            node_x_y = queue.popleft()
+            node, x, y = node_x_y[0], node_x_y[1], node_x_y[2]
+            col_rows[y].append((x, node.val))
+            if node.left:
+                queue.append((node.left, x + 1, y - 1))            
+            if node.right:
+                queue.append((node.right, x + 1, y + 1))
+
+        rst = []
+        min_col, max_col = min(col_rows.keys()), max(col_rows.keys())
+        for col in range(min_col, max_col + 1):
+            if col not in col_rows:
+                continue
+            col_rows[col].sort()
+            rst.append([t[1] for t in col_rows[col]])
+        return rst
