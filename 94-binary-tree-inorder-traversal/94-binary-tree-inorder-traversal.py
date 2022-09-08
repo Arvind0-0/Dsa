13473708cf622cb260e2382d623ff6a6ec8dbbe9
Arvind Singh <91238742+Arvind0-0@users.noreply.github.com>
2022-09-08 20:43:29 +0530

    Time: 56 ms (27.26%), Space: 13.8 MB (96.73%) - LeetHub

---
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -0,0 +1,6 @@
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right) if root else []
+
+
+        
