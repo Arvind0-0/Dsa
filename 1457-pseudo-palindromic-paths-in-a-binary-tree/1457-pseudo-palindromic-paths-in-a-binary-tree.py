9f7c364607af194e73993393d96527bfd2106263
Arvind Singh <91238742+Arvind0-0@users.noreply.github.com>
2022-09-14 19:57:41 +0530

    Time: 1765 ms (20.56%), Space: 85.9 MB (43.30%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -1,30 +1,49 @@
+'''
+class Solution:
+    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+        self.cnt = 0
+        
+        def isPalid(path):
+            md = set()
+            for p in path:
+                if p in md:
+                    md.remove(p)
+                else:
+                    md.add(p)
+            return True if len(md)<=1 else False
+        
+        def dfs(root, path):
+            if not root:
+                return
+            if not root.left and not root.right:
+                # res.append(path)
+                if isPalid(path):
+                    self.cnt += 1
+            if root.left:
+                dfs(root.left, path+[root.left.val])
+            if root.right:
+                dfs(root.right, path+[root.right.val])
+                
+        res = []
+        dfs(root, [root.val])
+                
+        return self.cnt 
+'''
 
 class Solution:
-    
-    def pseudoPalindromicPaths (self, root):
-        def helper(node):
-                if node==None:
-				# when we will call the left or right child of leaf node or our node will be empty
+    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+        self.cnt = 0
         
-                    return
-                dict[str(node.val)]+=1
-				#we will increase the occurece of digit by one 
-                if node.left==None and node.right==None:
-				#  this if condition is for the child nodes, where we will check whether it is palindromic or not
-                 
-                # here we are checking whether number of occurence of each integer is odd or even and counting the odd one, if we have odd occurence more than one it means it won't be palindromic so we are not increasing the ans value
-                    c=0
-                    for i in dict:
-                        if dict[i]%2!=0:
-                            c+=1
-                    if c<=1:
-                        ans[0]+=1
-                helper(node.left)
-                helper(node.right)
-                dict[str(node.val)]-=1
-        dict=defaultdict(lambda:0)
-		#for storing the occurence of each integer
-        ans=[0]
-        # storing the number of palindrome
-        helper(root)
-        return(ans[0])
+        def dfs(root, path):
+            if root:
+                path = path ^ (1 << root.val)
+                if not root.left and not root.right:
+                    if path & (path - 1) == 0:
+                        self.cnt += 1
+                else:
+                    dfs(root.left, path)
+                    dfs(root.right, path)
+                    
+        dfs(root, 0)
+        
+        return self.cnt
