c274cc70936ba44c9b101b031fc54a630edb8a77
Arvind Singh <91238742+Arvind0-0@users.noreply.github.com>
2022-10-18 23:31:11 +0530

    Added solution - LeetHub

---
+++ Print Diagonally - GFG/print-diagonally.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+def downwardDigonal(N, A): 
+    d = defaultdict(list)
+    for i in range(N):
+        for j in range(N):
+            d[i+j].append(A[i][j])
+    ans = []
+    for i in d:
+        for j in d[i]:
+            ans.append(j)
+    return ans
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        matrix =[]
+        for i in range(n):
+            row = list(map(int, input().strip().split()))
+            matrix.append(row)
+        ans = downwardDigonal(n,matrix)
+        for i in ans:
+            print(i,end=" ")
+        print()
+
+# } Driver Code Ends
