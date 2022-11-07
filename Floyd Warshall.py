72768c779d1910860c9f24a689f9bc85b88ac6d7
Arvind Singh <91238742+Arvind0-0@users.noreply.github.com>
2022-11-07 23:02:10 +0530

    Create Floyd Warshall.py

---
+++ Floyd Warshall.py
@@ -0,0 +1,26 @@
+class Solution:
+	def shortest_distance(self, matrix):
+		#Code here
+		n = len(matrix)
+		
+		for i in range(n):
+		    for j in range(n):
+		        if matrix[i][j]==-1:
+		            matrix[i][j]=10e9
+		        
+		        if i==j:
+		            matrix[i][j]==0
+	    
+	    
+	    for k in range(n):
+	        for i in range(n):
+	            for j in range(n):
+	                matrix[i][j]=min(matrix[i][j],matrix[i][k]+matrix[k][j])
+
+	    for i in range(n):
+		    for j in range(n):
+		        if matrix[i][j]==10e9:
+		            matrix[i][j]=-1	       
+	    
+	    
+	    #return matrix
