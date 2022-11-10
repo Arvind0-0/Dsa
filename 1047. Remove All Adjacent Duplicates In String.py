96e72afe187961d181b59e2030da46506ca104f2
Arvind Singh <91238742+Arvind0-0@users.noreply.github.com>
2022-11-10 07:43:52 +0530

    Create 1047. Remove All Adjacent Duplicates In String.py

---
+++ 1047. Remove All Adjacent Duplicates In String.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
+        for ch in s:                                    
+        if stack and stack[-1]==ch: stack.pop()               
+            else: stack.append(ch)                      
+        return ''.join(stack)  
